50ad82ef7b5c3ca680fce18b5ab101ef4682e6cb
John Doe <lukeschneider7@yahoo.com>
2024-06-28 09:42:03 -0400

    "changes to monte carlo.py"

--- montercarlo/montecarlo.py
+++ montercarlo/montecarlo.py
@@ -1,1 +1,16 @@
+import numpy as np
+import pandas as pd
 
+
+class Die:
+    def __init(self):
+        pass
+
+class Game:
+    def __init(self):
+        pass
+
+
+class Analyszer:
+    def __init(self):
+        pass
