f9a5b70d2320d0e051ac05ca34a678b20e886b35
John Doe <lukeschneider7@yahoo.com>
2024-07-07 13:23:57 -0400

    fp checkpoint 3

---
+++ package/__init__.py
@@ -0,0 +1,2 @@
+from . import montecarlo
+from . import montecarlo_test
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'package',
+    version = '0.1.0',
+    author = 'Luke Schneider',
+    author_email = 'vrd9sd@virginia.edu',
+    packages = find_packages(),
+    url = 'https://github.com/lukeschneider7/DS5100-FP1/tree/main/montercarlo',
+    license = 'LICENSE.txt',
+    description = 'An awesome package that does something',
+    long_description = open('README.txt').read(),
+    install_requires = ['numpy >= 1.11.1', 'matplotlib >= 1.5.1'],
+)
