ef9b20b23adf50f84708d4a67fb45122f318c8fc
John Doe <lukeschneider7@yahoo.com>
2024-07-08 08:20:18 -0400

    Finish the Die class

--- montecarlo/montecarlo.py
+++ montecarlo/montecarlo.py
@@ -1,13 +1,13 @@
 import numpy as np
 import pandas as pd
+import random
 
 
 class Die():
     def __init__(self, faces):
-        """ 
-        -1. Throw TypeError if not NumPy array, raise ValueError if array values not distinct
-        -3. Internally initialize weights to 1.0 for each face
-        -4. Save faces and weights in private df w/ faces in index
+        """ Internally initialize weights to 1 for each face
+        - Throw TypeError if not NumPy array, raise ValueError if array values not distinct
+        - Save faces and weights in private df w/ faces in index
         Args:
             faces: (array) NumPy array faces str/num dtype
         """
@@ -22,12 +22,11 @@ class Die():
         else: 
             weights = np.ones(faces.size)
             faces = faces.flatten()
-            self.df = pd.DataFrame({'values': weights}, index=faces) 
-            print(self.df)
+            self._df = pd.DataFrame({'values': weights}, index=faces) 
         
 
     def change_weight(self, face_to_change, weight_new ):
-        """Change weight of single die
+        """ Change weight of single face of die
         - Raise IndexError if face_value not in die array
         - Raise TypeError if weight_new is not valid type (int/float or castable as numeric)
         Args:
@@ -36,29 +35,28 @@ class Die():
         """
         if np.isin(face_to_change, self.faces)==False:
             raise IndexError("Face value not in die array")
-        
-        elif type(weight_new)!=float and int(weight_new)!= True and type(weight_new)!=int:
+        elif type(float(weight_new))!= float:
             raise TypeError("Weight_new arg must be int/float or castable as num")
-        
         else:
-            self.df.loc[face_to_change] = weight_new
-            print(self.df)
-        
+            self._df.loc[face_to_change] = weight_new
+            
 
     def roll_die(self, times=1):
-        """Roll die one or more times
+        """ Roll die one or more times, returning outcomes
         Args:
             times: (int) times die is to be rolled
         Returns:
-            outcomes: (list) Python list of outcomes
+            outcomes: (list) Python list of outcomes not internally stored
         """
-        pass
-    
-
-    def get_state(self):
-        pass
+        self._outcomes = random.choices(self._df.index.tolist(), weights=self._df['values'], k=times)
         
-
+    def get_state(self):
+        """ Returns a COPY of private die data frame
+        """
+        df_copy = self._df.copy()
+        return df_copy
+        #return self._outcomes
+        
 
 class Game():
     def __init(self):
@@ -72,7 +70,12 @@ class Analyszer():
 
 
 
-array = np.random.randint(0, 40, size=(2, 3))
-print(array)
+#array = np.random.randint(0, 40, size=(2, 3))
+array = np.array([
+    [1, 2, 3],
+    [4, 5, 6]
+])
 die = Die(array)
-die.change_weight(7, 'four')
+die.change_weight(5, 4)
+print(die.roll_die(3))
+print(die.get_state())
